79ddae8406078f60f132ac440ddf76d4b9862aec
Okoye Emmanuel .M <mmesoemma@gmail.com>
2023-01-15 22:46:29 +0100

    Updated the Clarifai API connection

--- controllers/image.js
+++ controllers/image.js
@@ -2,15 +2,25 @@ const Clarifai = require("clarifai");
 const { response } = require("express");
 
 const app = new Clarifai.App({
-    apiKey: '337453dc403e4bc1a51a58899c91a6c5'
+    apiKey: 'Enter your Api key'
 });
+
 const handleApiCall = (req, res) => {
     const { input } = req.body;
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, input)
+
+    app.models
+       .predict(
+        {
+            id: 'face-detection',
+            name: 'face-detection',
+            version: '6dc7e46bc9124c5c8824be4822abe105',
+            type: 'visual-detector',
+        }, input)
     .then(data => {
         res.json(data);
     })
     .catch(err => res.status(400).json("Unable to connect API"))
+
 }
 const handleImage = (req, res, db) => {
     const { id } = req.body;
